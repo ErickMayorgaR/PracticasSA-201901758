cb79f77952454d19b73111a3acbd001995d1eba9
ErickMayorgaR <3667150890101@ingenieria.usac.edu.gt>
2024-03-04 23:41:54 -0600

    Cambios Direccion Servicios Kubernetes

--- Practica2SA/consumirMicroservicio/index.js
+++ Practica2SA/consumirMicroservicio/index.js
@@ -6,8 +6,9 @@ const port = 3000;
 app.get('/info', async (req, res) => {
   const  name  = req.query.name;
   try {
-    const genderResponse = await axios.get(`http://localhost:3001/genderize?name=${name}`);
-    const ageResponse = await axios.get(`http://localhost:3002/agify?name=${name}`);
+    console.log("genderize y agify")
+    const genderResponse = await axios.get(`http://genderize-service:3001/genderize?name=${name}`);
+    const ageResponse = await axios.get(`http://agify-service:3002/agify?name=${name}`);
     res.json({
       name,
       genderize: genderResponse.data,
